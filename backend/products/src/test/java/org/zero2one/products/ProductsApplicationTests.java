1ee94ce2424d153ee2e80b36a5125fc25bc44a9b
suelenpaterno <suelen.paterno@gmail.com>
2019-06-04 19:34:34 -0300

    primeira aula backend

---
+++ backend/products/src/test/java/org/zero2one/products/ProductsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zero2one.products;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
